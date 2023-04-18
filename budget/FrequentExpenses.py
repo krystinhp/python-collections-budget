47f93972e6c6c4e057d11fafeaf050a4dbba329f
Krystin Hogan <krystin.hogan@wwt.com>
2023-04-18 12:07:53 -0500

    Completed Count Expenses by Category

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,8 +1,32 @@
 from .import Expense
+import collections
+import matplotlib.pyplot as plt
 
-expenses = Expense.Expense()
+#Read in the spending data
+expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
+#Create a list of the Spending Data
 spending_categories = []
 for expense in expenses.list:
     spending_categories.append(expense.category)
+
+#Count Categoies with a Counter Collection
+spending_counter = collections.Counter(spending_categories)
+print(spending_counter)
+
+#Get the Top 5 Collections
+top5 = spending_counter.most_common(5)
+
+#Convert the Dictonary to 2 List
+categories, count = zip(*top5) 
+
+#Plot the Top5 Most Common Categories
+fig, ax = plt.subplots()
+
+#Create the bar chart
+ax.bar(categories, count)
+ax.set_title("# of Purchases by Category")
+
+#Display the graph
+plt.show()
