bef93bbe69fc2efa4a84bae3dba5423be294fae7
Krystin Hogan <krystin.hogan@wwt.com>
2023-05-01 14:39:04 -0500

    ExpensesCategories4

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -17,21 +17,21 @@ def main():
             print("Sets are NOT equal by subset test")
 
 print(timeit.timeit(stmt =  "expenses.categorize_for_loop()",
-               setup = '''
-               from . import Expense
-               expenses = Expense.Expenses()
-               expenses.read_expenses('data/spending_data.csv')''',
-               number=100000, 
-               globals=globals() )
+            setup = '''
+            from . import Expense
+            expenses = Expense.Expenses()
+            expenses.read_expenses('data/spending_data.csv')''',
+            number=100000, 
+            globals=globals() )
 )
 
 print(timeit.timeit(stmt = "expenses.categorize_set_comprehension()",
-               setup = '''
-               from . import Expense
-               expenses = Expense.Expenses()
-               expenses.read_expenses('data/spending_data.csv')''',
-               number=100000, 
-               globals=globals() )
+            setup = '''
+            from . import Expense
+            expenses = Expense.Expenses()
+            expenses.read_expenses('data/spending_data.csv')''',
+            number=100000, 
+            globals=globals() )
 ) 
 
 fig,ax=plt.subplots()
@@ -51,4 +51,3 @@ plt.show()
 
 if __name__ == "__main__":
     main()
-
