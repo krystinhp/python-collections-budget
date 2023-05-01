dd3635143dcdc1b0c93850d052f565d636ed201a
Krystin Hogan <krystin.hogan@wwt.com>
2023-05-01 08:27:46 -0500

    Categorizing expeneses5

--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -6,11 +6,10 @@ def main():
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
 
-divided_set_comp = expenses.categorize_set_comprehension()
+    divided_set_comp = expenses.categorize_set_comprehension()
+    if divided_set_comp != divided_for_loop:
+        print('Sets are NOT equal by == test')
 
-if divided_set_comp != divided_for_loop:
-    print('Sets are NOT equal by == test')
-    
 if __name__ == "__main__":
     main()
 
