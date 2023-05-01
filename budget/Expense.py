8583187fba3b2d1851a4dadd127b79d693e468fb
Krystin Hogan <krystin.hogan@wwt.com>
2023-05-01 08:12:35 -0500

    Categorizing expeneses3

--- budget/Expense.py
+++ budget/Expense.py
@@ -42,20 +42,18 @@ class Expenses():
             return [necessary_expenses, food_expenses, unnecessary_expenses]
     
     def categorize_set_comprehension(self):
-        necessary_expenses = {
-            x for x in self.list}
-        if :
-            x.category == 'Phone' or \
-            x.category == 'Auto or Gas' or \
-            x.category == 'Classes' or \
-            x.category == 'Utilities' or \ 
-            x.category == 'Mortgage'
+        necessary_expenses = set()
+        for x in self.list:
+            if (x.category == 'Phone' or 
+                x.category == 'Auto or Gas' or 
+                x.category == 'Classes' or 
+                x.category == 'Utilities' or 
+                x.category == 'Mortgage'):
 
-        food_expenses = {
-            x for x in self.list}
-        if :
-            x.category = 'Groceries' or \
-            x.category = 'Eating Out'
+        food_expenses = set()
+        for x in self.list:
+            if (x.category == 'Groceries' or 
+               x.category == 'Eating Out'):
         
         unnecessary_expenses = set(self.list)
         
